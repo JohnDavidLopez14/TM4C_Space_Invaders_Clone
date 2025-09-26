09bcf8ed97b8ee17d0970c16191bc933950f68ff
johndavidlopez14 <johnd.lopez14@gmail.com>
2025-09-25 20:51:20 -0700

    moved bitmaps into bitmaps.h

---
+++ bitmaps.h
@@ -0,0 +1,32 @@
+#ifndef __bitmaps__
+#define __bitmaps__
+
+typedef struct {
+    const unsigned char *bmp;
+    int width;
+    int height;
+} Bitmap;
+
+extern const Bitmap smallEnemy30PointA;
+extern const Bitmap smallEnemy30PointB;
+extern const Bitmap smallEnemy20PointA;
+extern const Bitmap smallEnemy20PointB;
+extern const Bitmap smallEnemy10PointA;
+extern const Bitmap smallEnemy10PointB;
+extern const Bitmap playerShip0;
+extern const Bitmap smallEnemeyBonus0;
+extern const Bitmap bunker0;
+extern const Bitmap bunker1;
+extern const Bitmap bunker2;
+extern const Bitmap bunker3;
+extern const Bitmap bigExplosion0;
+extern const Bitmap bigExplosion1;
+extern const Bitmap smallExplosion0;
+extern const Bitmap smallExplosion1;
+extern const Bitmap missile0;
+extern const Bitmap missile1;
+extern const Bitmap missile2;
+extern const Bitmap laser0;
+extern const Bitmap laser1;
+
+#endif
